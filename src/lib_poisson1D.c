6e69585ec711cbe42da54616125603b94573875d
shani <c.ztoti.2001.03@gmail.com>
2023-12-16 09:28:47 +0100

    Implémentation de la fonction d'initialisation de la matrice bande pour l'équation de Poisson 1D
    -> set_GB_operator_colMajor_poissonD ( lib_poissonD.c )

--- src/lib_poisson1D.c
+++ src/lib_poisson1D.c
@@ -1,120 +1,169 @@
 /**********************************************/
 /* lib_poisson1D.c                            */
-/* Numerical library developed to solve 1D    */ 
+/* Numerical library developed to solve 1D    */
 /* Poisson problem (Heat equation)            */
 /**********************************************/
 #include "lib_poisson1D.h"
 
-void set_GB_operator_colMajor_poisson1D(double* AB, int *lab, int *la, int *kv){
-}
+void set_GB_operator_colMajor_poisson1D(double *AB, int *lab, int *la, int *kv) {
+    // lab = nombre de colonnes
+    // la = taille d'une ligne
+    // kv => première ligne de la matrice bande pour la factorisation LU
 
-void set_GB_operator_colMajor_poisson1D_Id(double* AB, int *lab, int *la, int *kv){
-}
+    // Matrice v0 v1 .. u d l
+    size_t taille = (*lab) * (*la);
+    for (int indice = 0; indice < taille; indice += *lab) {
 
-void set_dense_RHS_DBC_1D(double* RHS, int* la, double* BC0, double* BC1){
-}  
-
-void set_analytical_solution_DBC_1D(double* EX_SOL, double* X, int* la, double* BC0, double* BC1){
-}  
-
-void set_grid_points_1D(double* x, int* la){
-}
-
-void write_GB_operator_rowMajor_poisson1D(double* AB, int* lab, int* la, char* filename){
-  FILE * file;
-  int ii,jj;
-  file = fopen(filename, "w");
-  //Numbering from 1 to la
-  if (file != NULL){
-    for (ii=0;ii<(*lab);ii++){
-      for (jj=0;jj<(*la);jj++){
-	fprintf(file,"%lf\t",AB[ii*(*la)+jj]);
+      for (int i = 0; i < *kv; ++i) {
+        AB[indice + i] = 0.0;
       }
-      fprintf(file,"\n");
+      // Assignation des valeurs U D L de la matrice poisson
+      AB[indice + *kv] = -1.0;
+      AB[indice + (*kv) + 1] = 2.0;
+      AB[indice + (*kv) + 2] = -1.0;
     }
-    fclose(file);
-  }
-  else{
-    perror(filename);
-  }
+    // Remplace les extrémités de la matrice par 0.0
+    AB[*kv] = 0.0;
+    AB[taille - 1] = 0.0;
 }
 
-void write_GB_operator_colMajor_poisson1D(double* AB, int* lab, int* la, char* filename){
-  FILE * file;
-  int ii,jj;
-  file = fopen(filename, "w");
-  //Numbering from 1 to la
-  if (file != NULL){
-    for (ii=0;ii<(*la);ii++){
-      for (jj=0;jj<(*lab);jj++){
-	fprintf(file,"%lf\t",AB[ii*(*lab)+jj]);
-      }
-      fprintf(file,"\n");
-    }
-    fclose(file);
-  }
-  else{
-    perror(filename);
-  }
+void set_GB_operator_colMajor_poisson1D_Id(double *AB, int *lab, int *la, int *kv)
+{
 }
 
-void write_GB2AIJ_operator_poisson1D(double* AB, int* la, char* filename){
-  FILE * file;
-  int jj;
-  file = fopen(filename, "w");
-  //Numbering from 1 to la
-  if (file != NULL){
-    for (jj=1;jj<(*la);jj++){
-      fprintf(file,"%d\t%d\t%lf\n",jj,jj+1,AB[(*la)+jj]);
-    }
-    for (jj=0;jj<(*la);jj++){
-      fprintf(file,"%d\t%d\t%lf\n",jj+1,jj+1,AB[2*(*la)+jj]);
-    }
-    for (jj=0;jj<(*la)-1;jj++){
-      fprintf(file,"%d\t%d\t%lf\n",jj+2,jj+1,AB[3*(*la)+jj]);
-    }
-    fclose(file);
-  }
-  else{
-    perror(filename);
-  }
+void set_dense_RHS_DBC_1D(double *RHS, int *la, double *BC0, double *BC1)
+{
 }
 
-void write_vec(double* vec, int* la, char* filename){
-  int jj;
-  FILE * file;
+void set_analytical_solution_DBC_1D(double *EX_SOL, double *X, int *la, double *BC0, double *BC1)
+{
+}
+
+void set_grid_points_1D(double *x, int *la)
+{
+}
+
+void write_GB_operator_rowMajor_poisson1D(double *AB, int *lab, int *la, char *filename)
+{
+  FILE *file;
+  int ii, jj;
   file = fopen(filename, "w");
   // Numbering from 1 to la
-  if (file != NULL){
-    for (jj=0;jj<(*la);jj++){
-      fprintf(file,"%lf\n",vec[jj]);
+  if (file != NULL)
+  {
+    for (ii = 0; ii < (*lab); ii++)
+    {
+      for (jj = 0; jj < (*la); jj++)
+      {
+        fprintf(file, "%lf\t", AB[ii * (*la) + jj]);
+      }
+      fprintf(file, "\n");
     }
     fclose(file);
   }
-  else{
+  else
+  {
     perror(filename);
-  } 
-}  
+  }
+}
 
-void write_xy(double* vec, double* x, int* la, char* filename){
-  int jj;
-  FILE * file;
+void write_GB_operator_colMajor_poisson1D(double *AB, int *lab, int *la, char *filename)
+{
+  FILE *file;
+  int ii, jj;
   file = fopen(filename, "w");
   // Numbering from 1 to la
-  if (file != NULL){
-    for (jj=0;jj<(*la);jj++){
-      fprintf(file,"%lf\t%lf\n",x[jj],vec[jj]);
+  if (file != NULL)
+  {
+    for (ii = 0; ii < (*la); ii++)
+    {
+      for (jj = 0; jj < (*lab); jj++)
+      {
+        fprintf(file, "%lf\t", AB[ii * (*lab) + jj]);
+      }
+      fprintf(file, "\n");
     }
     fclose(file);
   }
-  else{
+  else
+  {
     perror(filename);
-  } 
-}  
+  }
+}
 
-int indexABCol(int i, int j, int *lab){
+void write_GB2AIJ_operator_poisson1D(double *AB, int *la, char *filename)
+{
+  FILE *file;
+  int jj;
+  file = fopen(filename, "w");
+  // Numbering from 1 to la
+  if (file != NULL)
+  {
+    for (jj = 1; jj < (*la); jj++)
+    {
+      fprintf(file, "%d\t%d\t%lf\n", jj, jj + 1, AB[(*la) + jj]);
+    }
+    for (jj = 0; jj < (*la); jj++)
+    {
+      fprintf(file, "%d\t%d\t%lf\n", jj + 1, jj + 1, AB[2 * (*la) + jj]);
+    }
+    for (jj = 0; jj < (*la) - 1; jj++)
+    {
+      fprintf(file, "%d\t%d\t%lf\n", jj + 2, jj + 1, AB[3 * (*la) + jj]);
+    }
+    fclose(file);
+  }
+  else
+  {
+    perror(filename);
+  }
+}
+
+void write_vec(double *vec, int *la, char *filename)
+{
+  int jj;
+  FILE *file;
+  file = fopen(filename, "w");
+  // Numbering from 1 to la
+  if (file != NULL)
+  {
+    for (jj = 0; jj < (*la); jj++)
+    {
+      fprintf(file, "%lf\n", vec[jj]);
+    }
+    fclose(file);
+  }
+  else
+  {
+    perror(filename);
+  }
+}
+
+void write_xy(double *vec, double *x, int *la, char *filename)
+{
+  int jj;
+  FILE *file;
+  file = fopen(filename, "w");
+  // Numbering from 1 to la
+  if (file != NULL)
+  {
+    for (jj = 0; jj < (*la); jj++)
+    {
+      fprintf(file, "%lf\t%lf\n", x[jj], vec[jj]);
+    }
+    fclose(file);
+  }
+  else
+  {
+    perror(filename);
+  }
+}
+
+int indexABCol(int i, int j, int *lab)
+{
   return 0;
 }
-int dgbtrftridiag(int *la, int*n, int *kl, int *ku, double *AB, int *lab, int *ipiv, int *info){
+int dgbtrftridiag(int *la, int *n, int *kl, int *ku, double *AB, int *lab, int *ipiv, int *info)
+{
   return *info;
 }
